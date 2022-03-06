37be1f49b0b96846dfe6348eb1caa3c5e92b0670
NDJSec <nicolas.d.janis@gmail.com>
2022-03-05 21:15:31 -0500

    Add portscan.go

--- go.mod
+++ go.mod
@@ -1,3 +1,7 @@
 module github.com/NDJSec/Go_ShadowFramework
 
 go 1.17
+
+require (
+    golang.org/x/sync v0.0.0-20210220032951-036812b2e83c 
+)
---
+++ modules/auxiliary/portscan/portscan.go
@@ -0,0 +1,59 @@
+package portscan
+
+import (
+	"fmt"
+	"net"
+	"os/exec"
+	"strconv"
+	"strings"
+	"time"
+
+	"golang.org/x/sync/semaphore"
+)
+
+type PortScanner struct {
+	ip   string
+	lock *semaphore.Weighted
+}
+
+func Init() {
+
+}
+
+func Ulimit() int64 {
+	out, err := exec.Command("ulimit", "-n").Output()
+	if err != nil {
+		panic(err)
+	}
+
+	s := strings.TrimSpace(string(out))
+
+	i, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		panic(err)
+	}
+
+	return i
+}
+
+func ScanPort(ip string, port int, timeout time.Duration) {
+	target := fmt.Sprintf("%s:%d", ip, port)
+	conn, err := net.DialTimeout("tcp", target, timeout)
+
+	if err != nil {
+		if strings.Contains(err.Error(), "too many open files") {
+			time.Sleep(timeout)
+			ScanPort(ip, port, timeout)
+		} else {
+			fmt.Println(port, "closed")
+		}
+		return
+	}
+
+	conn.Close()
+	fmt.Println(port, "open")
+}
+
+func portscan() {
+
+}
